e30192c2cf2d9fe618d8ade01961d7dc62b29f18
Kasper Munch <kaspermunch@birc.au.dk>
2024-11-01 15:44:57 +0100

    Initial commit

---
+++ libraryname/__init__.py
@@ -0,0 +1,33 @@
+
+def foo(x: int=1, y: int=1) -> int:
+    """
+    The sum of two numbers.
+
+    Parameters
+    ----------
+    x : 
+        The first number
+    y :
+        The second number.
+
+    Returns
+    -------
+    :
+        Sum of two numbers.
+
+    Examples
+    --------
+    
+    Adding two numbers
+    
+    ```python
+    result = foo(2, 4)
+    ```
+    
+    See Also
+    --------
+    [](`libraryname.scripts.bar`), [](`libraryname.foo`)
+    """
+    
+    return x + y
+
